8b1735440e03db515c2c4ef74e191c5ae006faed
ARNOLDT20 <atarimo117@gmail.com>
2026-07-08 20:08:30 +0300

    jhh

--- mobile-app/www/app.js
+++ mobile-app/www/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const splash = document.getElementById('splash');
-  const url = 'https://royal.t20tech.site/index.php';
+    const splash = document.getElementById('splash');
+    const url = 'https://royal.t20tech.site/index.php';
 
-  window.location.replace(url);
-  setTimeout(() => splash.classList.add('hidden'), 800);
+    window.location.replace(url);
+    setTimeout(() => splash.classList.add('hidden'), 800);
 });
